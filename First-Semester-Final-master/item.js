ba32db6923e6ab89d165bbe33b0ab42f2ca8e4c7
DanyloKuleshko <7dkuleshko@stpatrick.org>
2016-12-12 10:53:29 -0600

    Add files via upload

---
+++ First-Semester-Final-master/item.js
@@ -0,0 +1,10 @@
+class Item{
+//Fill in the item class below this comment
+  constructor (name, price, shipping){
+  this.name = name;
+  this.price = price;
+  this.shipping = shipping;
+  }
+}
+//Create your three test items below this comment.
+NoahItem = new Item(FloweVase )
